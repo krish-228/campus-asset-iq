ba9ddd464c47fe72f2a1b8748b4651cac14fe847
Krish <krish2282003@gmail.com>
2026-09-17 11:48:34 +0530

    Fix build.sh for Render build container

--- build.sh
+++ build.sh
@@ -3,10 +3,4 @@
 set -o errexit
 
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
-
-if [ -n "$DATABASE_URL" ]; then
-    python manage.py migrate --noinput
-    python manage.py loaddata campus_data.json || true
-fi
